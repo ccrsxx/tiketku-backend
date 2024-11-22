b91c8684174b27e3ba652cef4c585b833e7b2c65
ccrsxx <aminrisal@gmail.com>
2024-11-22 23:45:52 +0700

    fix: ci error

--- src/middlewares/validation/__test__/user.test.js
+++ src/middlewares/validation/__test__/user.test.js
@@ -19,50 +19,6 @@ const { UserValidationMiddleware } =
   );
 
 describe('User validation middleware', () => {
-  describe('Is valid user payload', () => {
-    it('should call next() if the payload is valid', () => {
-      const { req, res, next } = setupExpressMock({
-        req: {
-          body: {
-            name: 'Test User',
-            email: 'test@email.com',
-            address: 'Test Address',
-            password: 'password',
-            identityType: 'KTP',
-            identityNumber: '1234567890'
-          },
-          params: {
-            id: '4370e6f4-fdc1-47e4-917b-fa56d83f94c5'
-          }
-        }
-      });
-
-      UserValidationMiddleware.isValidUserPayload(req, res, next);
-
-      expect(next).toHaveBeenCalledTimes(1);
-    });
-
-    it('should return 400 http error if the payload is invalid', async () => {
-      const { req, res, next } = setupExpressMock({
-        req: {
-          body: {
-            name: 'Test User'
-          }
-        }
-      });
-
-      const error = await getFunctionThrownError(() =>
-        UserValidationMiddleware.isValidUserPayload(req, res, next)
-      );
-
-      expect(error).toBeInstanceOf(HttpError);
-      expect(error).toHaveProperty('statusCode', 400);
-      expect(error).toHaveProperty('message', '"email" is required');
-
-      expect(next).not.toHaveBeenCalled();
-    });
-  });
-
   describe('Is valid params user id', () => {
     it('should call next() if the params id is valid', () => {
       const { req, res, next } = setupExpressMock({
