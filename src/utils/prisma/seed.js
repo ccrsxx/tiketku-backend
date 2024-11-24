4683b154bc7ba3b1e674e25272e318ccbdd02342
ccrsxx <aminrisal@gmail.com>
2024-11-24 21:03:13 +0700

    feat: add seeder for database

--- src/utils/prisma/seed.js
+++ src/utils/prisma/seed.js
@@ -1,1 +1,16 @@
-// TODO: add seed later
+import { prisma } from '../db.js';
+import { logger } from '../../loaders/pino.js';
+
+async function main() {
+  try {
+    // TODO: Add your seed data here
+
+    await prisma.$disconnect();
+  } catch (err) {
+    logger.error(err);
+    await prisma.$disconnect();
+    process.exit(1);
+  }
+}
+
+void main();
