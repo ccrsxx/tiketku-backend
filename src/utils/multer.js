59728c01db0f3a78bc8890e15ac536f55b82a1b4
ccrsxx <aminrisal@gmail.com>
2024-11-28 12:53:09 +0700

    feat: add image extension validation on multer

--- src/utils/multer.js
+++ src/utils/multer.js
@@ -1,9 +1,24 @@
 import multer from 'multer';
+import { HttpError } from './error.js';
 
 const storage = multer.memoryStorage();
 
+const ALLOWED_IMAGE_EXTENSIONS = ['jpg', 'jpeg', 'png', 'gif'];
+
 export const uploadToMemory = multer({
   storage,
+  fileFilter: (_req, file, cb) => {
+    const fileExtension = file?.originalname?.split('.').pop();
+
+    const validExtension =
+      fileExtension && ALLOWED_IMAGE_EXTENSIONS.includes(fileExtension);
+
+    if (!validExtension) {
+      return cb(new HttpError(400, { message: 'Only images are allowed' }));
+    }
+
+    cb(null, true);
+  },
   limits: {
     fileSize: 1024 * 1024 * 10 // 10MB
   }
