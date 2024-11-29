7e40f58de8ea33a35505dde646adba2b2c424b4b
dindalolyta23 <dindalolyta@students.amikom.ac.id>
2024-11-29 18:03:46 +0700

    feat: add get airport endpoint

--- src/controllers/airport.js
+++ src/controllers/airport.js
@@ -1,29 +1,16 @@
 import { AirportService } from '../services/airport.js'; // Import layanan yang relevan
 
 /**
+ * Mendapatkan semua bandara dan mengembalikan respons JSON
+ *
  * @param {import('express').Request} _req
  * @param {import('express').Response} res
  */
 async function getAirports(_req, res) {
-  try {
-    const airports = await AirportService.getAirports();
-    res.status(200).json({ data: airports });
-  } catch (error) {
-    // Menangani error secara lebih baik
-    if (error instanceof Error) {
-      res
-        .status(500)
-        .json({ message: error.message || 'Internal Server Error' });
-    } else {
-      res.status(500).json({ message: 'An unknown error occurred' });
-    }
-  }
+  const airports = await AirportService.getAirports();
+  res.status(200).json({ data: airports });
 }
 
-/**
- * @param {import('express').Request} _req
- * @param {import('express').Response} res
- */
 export const AirportController = {
   getAirports
 };
--- src/services/airport.js
+++ src/services/airport.js
@@ -1,27 +1,15 @@
-import { PrismaClient } from '@prisma/client'; // Import Prisma Client
+import { prisma } from '../utils/db.js'; // Mengimpor Prisma dari utils/db.js
 
-const prisma = new PrismaClient();
-
-class AirportService {
-  /**
-   * Mendapatkan semua bandara
-   *
-   * @returns {Promise<import('@prisma/client').Airport[]>}
-   */
-  static async getAirports() {
-    try {
-      const airports = await prisma.airport.findMany();
-      return airports;
-    } catch (error) {
-      // Memastikan error adalah instance dari Error
-      if (error instanceof Error) {
-        throw new Error(error.message || 'Failed to get airports');
-      } else {
-        // Jika bukan instance Error, kita bisa berikan pesan default
-        throw new Error('Unknown error occurred while fetching airports');
-      }
-    }
-  }
+/**
+ * Mendapatkan semua bandara
+ *
+ * @returns {Promise<import('@prisma/client').Airport[]>}
+ */
+async function getAirports() {
+  // Menggunakan instance prisma untuk mengambil data bandara
+  return prisma.airport.findMany();
 }
 
-export { AirportService };
+export const AirportService = {
+  getAirports
+};
