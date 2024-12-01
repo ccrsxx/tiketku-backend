19a1cec58d9f1f89833323522334a4a1e3d9864c
ccrsxx <aminrisal@gmail.com>
2024-12-01 23:32:41 +0700

    feat: improve seed to support round trip flight

--- src/utils/prisma/seed/schema/flight.js
+++ src/utils/prisma/seed/schema/flight.js
@@ -3,12 +3,15 @@ import { faker } from '@faker-js/faker';
 import { logger } from '../../../../loaders/pino.js';
 import { FlightClassType } from '@prisma/client';
 
-/** @import {Prisma,Airport} from '@prisma/client' */
+/** @import {Prisma,Airport,Airline,Airplane} from '@prisma/client' */
 
 export async function seedFlight() {
   const seedEvent = await prisma.event.findFirst({
     where: {
-      type: 'SEED_FLIGHT'
+      type: 'SEED_FLIGHT',
+      expiredAt: {
+        not: null
+      }
     },
     select: {
       expiredAt: true
@@ -25,7 +28,13 @@ export async function seedFlight() {
     const { expiredAt } = seedEvent;
 
     if (!expiredAt) shouldSeed = false;
-    else shouldSeed = new Date() > expiredAt;
+    else {
+      logger.info(
+        `Last flight seed was at ${expiredAt.toLocaleDateString()} ${expiredAt.toLocaleTimeString()}.`
+      );
+
+      shouldSeed = new Date() > expiredAt;
+    }
   }
 
   if (!shouldSeed) {
@@ -47,76 +56,50 @@ export async function seedFlight() {
     );
   }
 
-  const flightClassType = Object.values(FlightClassType);
-
+  // Add +1 day from current date for the next 7 days
   const futureFlightDates = Array.from({ length: 7 }, (_, i) =>
     faker.date.soon({ days: i + 1 })
   );
 
   /** @type {Prisma.FlightCreateManyInput[]} */
-  const flights = [];
+  const normalFlights = [];
 
-  const MAX_DAILY_FLIGHTS = 5;
+  const MAX_DAILY_FLIGHTS = 3;
 
   for (const flightDate of futureFlightDates) {
     for (const _ of Array.from({ length: MAX_DAILY_FLIGHTS })) {
-      // Randomly select unique airports
-      const departureAirport = faker.helpers.arrayElement(airports);
-
-      /** @type {Airport} */
-      let destinationAirport;
-
-      // Ensure destination airport is different from departure airport
-      do {
-        destinationAirport = faker.helpers.arrayElement(airports);
-      } while (destinationAirport.id === departureAirport.id);
-
-      const airline = faker.helpers.arrayElement(airlines);
-      const airplane = faker.helpers.arrayElement(airplanes);
-      const flightClass = faker.helpers.arrayElement(flightClassType);
-
-      // Generate random departure and arrival times on same day
-      const departureTimestamp = roundToNearestFiveMinutes(
-        faker.date.between({
-          from: flightDate,
-          to: faker.date.soon({ refDate: flightDate, days: 1 })
-        })
-      );
-
-      let arrivalTimestamp = new Date(departureTimestamp);
-
-      // Add random 1-5 hours to arrival time
-      arrivalTimestamp.setHours(
-        arrivalTimestamp.getHours() + faker.number.int({ min: 1, max: 6 })
-      );
-
-      // Add random 0-60 minutes to arrival time
-      arrivalTimestamp.setMinutes(
-        arrivalTimestamp.getMinutes() + faker.number.int({ min: 0, max: 60 })
-      );
-
-      arrivalTimestamp = roundToNearestFiveMinutes(arrivalTimestamp);
-
-      // Add random price for flight
-      const flightPrice = faker.number.int({ min: 800_000, max: 10_000_000 });
-
-      // Add probability of 20% for discount
-      const discount = faker.datatype.boolean({ probability: 0.2 })
-        ? faker.number.int({ min: 5, max: 50 })
-        : null;
-
-      flights.push({
-        id: crypto.randomUUID(),
-        type: flightClass,
-        price: flightPrice,
-        discount: discount,
-        airlineId: airline.id,
-        airplaneId: airplane.id,
-        arrivalTimestamp: arrivalTimestamp,
-        departureTimestamp: departureTimestamp,
-        departureAirportId: departureAirport.id,
-        destinationAirportId: destinationAirport.id
+      const randomNormalFlight = createFlight(flightDate, {
+        airlines,
+        airports,
+        airplanes
       });
+
+      normalFlights.push(randomNormalFlight);
+    }
+  }
+
+  /** @type {Prisma.FlightCreateManyInput[]} */
+  const roundTripFlights = [];
+
+  // Add +1 day from normal flights for the next 7 days
+  const roundTripFlightDates = Array.from({ length: 7 }, (_, i) =>
+    faker.date.soon({ days: i + 2 })
+  );
+
+  for (const normalFlight of normalFlights) {
+    const { departureAirportId, destinationAirportId } = normalFlight;
+
+    for (const flightDate of roundTripFlightDates) {
+      const roundTripFlight = createFlight(flightDate, {
+        airlines,
+        airports,
+        airplanes
+      });
+
+      roundTripFlight.departureAirportId = destinationAirportId;
+      roundTripFlight.destinationAirportId = departureAirportId;
+
+      roundTripFlights.push(roundTripFlight);
     }
   }
 
@@ -132,7 +115,10 @@ export async function seedFlight() {
       }
     }),
     prisma.flight.createMany({
-      data: flights
+      data: normalFlights
+    }),
+    prisma.flight.createMany({
+      data: roundTripFlights
     })
   ]);
 }
@@ -149,4 +135,80 @@ function roundToNearestFiveMinutes(date) {
   date.setMinutes(roundedMinutes, 0, 0);
 
   return date;
+}
+
+/**
+ * @typedef {Object} NeededModels
+ * @property {Airline[]} airlines
+ * @property {Airport[]} airports
+ * @property {Airplane[]} airplanes
+ */
+
+/**
+ * @param {Date} flightDate
+ * @param {NeededModels} models
+ * @returns {Prisma.FlightCreateManyInput}
+ */
+function createFlight(flightDate, models) {
+  const { airlines, airports, airplanes } = models;
+
+  // Randomly select unique departure airports
+  const departureAirport = faker.helpers.arrayElement(airports);
+
+  /** @type {Airport} */
+  let destinationAirport;
+
+  // Ensure destination airport is different from departure airport
+  do {
+    destinationAirport = faker.helpers.arrayElement(airports);
+  } while (destinationAirport.id === departureAirport.id);
+
+  const flightClassType = Object.values(FlightClassType);
+
+  const airline = faker.helpers.arrayElement(airlines);
+  const airplane = faker.helpers.arrayElement(airplanes);
+  const flightClass = faker.helpers.arrayElement(flightClassType);
+
+  // Generate random departure and arrival times on same day
+  const departureTimestamp = roundToNearestFiveMinutes(
+    faker.date.between({
+      from: flightDate,
+      to: faker.date.soon({ refDate: flightDate, days: 1 })
+    })
+  );
+
+  let arrivalTimestamp = new Date(departureTimestamp);
+
+  // Add random 1-5 hours to arrival time
+  arrivalTimestamp.setHours(
+    arrivalTimestamp.getHours() + faker.number.int({ min: 1, max: 6 })
+  );
+
+  // Add random 0-60 minutes to arrival time
+  arrivalTimestamp.setMinutes(
+    arrivalTimestamp.getMinutes() + faker.number.int({ min: 0, max: 60 })
+  );
+
+  arrivalTimestamp = roundToNearestFiveMinutes(arrivalTimestamp);
+
+  // Add random price for flight
+  const flightPrice = faker.number.int({ min: 800_000, max: 10_000_000 });
+
+  // Add probability of 20% for discount
+  const discount = faker.datatype.boolean({ probability: 0.2 })
+    ? faker.number.int({ min: 5, max: 50 })
+    : null;
+
+  return {
+    id: crypto.randomUUID(),
+    type: flightClass,
+    price: flightPrice,
+    discount: discount,
+    airlineId: airline.id,
+    airplaneId: airplane.id,
+    arrivalTimestamp: arrivalTimestamp,
+    departureTimestamp: departureTimestamp,
+    departureAirportId: departureAirport.id,
+    destinationAirportId: destinationAirport.id
+  };
 }
