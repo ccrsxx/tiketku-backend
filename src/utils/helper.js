cc089effdacd97e7d7df6ff4f3aeff7c11ae4334
ccrsxx <aminrisal@gmail.com>
2024-12-08 14:03:58 +0700

    refactor: remove unused function

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,5 +1,4 @@
 import { randomBytes, randomInt } from 'crypto';
-import { validPageCountSchema } from './validation.js';
 
 /** @returns {string} */
 export function generateRandomToken(size = 12) {
@@ -20,53 +19,4 @@ export function toTitleCase(str) {
     /\w\S*/g,
     (text) => text.charAt(0).toUpperCase() + text.substring(1).toLowerCase()
   );
-}
-
-/**
- * @typedef {Object} PaginationMeta
- * @property {number} page
- * @property {number} limit
- * @property {number} pageCount
- * @property {number} recordCount
- */
-
-/**
- * @typedef {Object} GeneratedPaginationMeta
- * @property {number} limit
- * @property {number} offset
- * @property {boolean} offPageLimit
- * @property {PaginationMeta} meta
- */
-
-/**
- * @typedef {Object} PaginationMetaOptions
- * @property {number | null | undefined} page
- * @property {number} limit
- * @property {number} recordCount
- */
-
-/**
- * @param {PaginationMetaOptions} options
- * @returns {GeneratedPaginationMeta}
- */
-export function generatePaginationMeta({ page, limit, recordCount }) {
-  const parsedPage = validPageCountSchema.safeParse(page).data ?? 1;
-
-  const pageCount = Math.ceil(recordCount / limit);
-
-  const offset = (parsedPage - 1) * limit;
-
-  const offPageLimit = parsedPage > pageCount;
-
-  return {
-    limit,
-    offset,
-    offPageLimit,
-    meta: {
-      page: parsedPage,
-      limit,
-      pageCount,
-      recordCount
-    }
-  };
 }
