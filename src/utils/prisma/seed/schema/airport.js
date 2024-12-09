205417f10837d8ee8958de9caa9cb4fcea886442
niemoo <izzanabdul123@gmail.com>
2024-12-09 14:16:10 +0700

    feat: update seed image

--- src/utils/prisma/seed/schema/airport.js
+++ src/utils/prisma/seed/schema/airport.js
@@ -11,7 +11,8 @@ export async function seedAirport() {
       city: 'Yogyakarta',
       code: 'YIA',
       type: 'DOMESTIC',
-      image: 'https://ik.imagekit.io/iaqozxfxq/IMG-1718886705943_ht2QKyRGM.jpg',
+      image:
+        'https://storage.cloud.google.com/plane-ticket-be.firebasestorage.app/public/1c94f349-1697-4377-80a1-efdbb85b78af.jpg',
       continent: 'ASIA'
     },
     {
@@ -20,7 +21,8 @@ export async function seedAirport() {
       city: 'Jakarta',
       code: 'CGK',
       type: 'DOMESTIC',
-      image: 'https://ik.imagekit.io/iaqozxfxq/IMG-1718888358211_jRGb4UMuu.jpg',
+      image:
+        'https://storage.cloud.google.com/plane-ticket-be.firebasestorage.app/public/4b1110ae-28f8-4287-97a1-408ea3916758.jpg',
       continent: 'ASIA'
     },
     {
@@ -29,7 +31,8 @@ export async function seedAirport() {
       city: 'Makassar',
       code: 'UPG',
       type: 'DOMESTIC',
-      image: 'https://ik.imagekit.io/iaqozxfxq/IMG-1718978705711_FNExtTfDv.jpg',
+      image:
+        'https://storage.cloud.google.com/plane-ticket-be.firebasestorage.app/public/f5119606-fd00-4257-9591-a1f6bc0a0506.jpg',
       continent: 'ASIA'
     },
     {
@@ -37,45 +40,49 @@ export async function seedAirport() {
       name: 'Fatmawati Soekarno',
       city: 'Bengkulu',
       code: 'BKS',
-      image:
-        'https://ik.imagekit.io/iaqozxfxq/IMG-1719062019111_b1PBlRDVt.jpeg',
       type: 'DOMESTIC',
+      image:
+        'https://storage.cloud.google.com/plane-ticket-be.firebasestorage.app/public/6cb364ba-f770-435b-bd0a-5dceff41f564.jpg',
       continent: 'ASIA'
     },
     {
       id: 'c6d0564e-706a-40b5-b54f-859ad563ae98',
       name: 'Juanda International Airport',
       city: 'Surabaya',
-      code: ' SUB',
+      code: 'SUB',
       type: 'DOMESTIC',
-      image: 'https://ik.imagekit.io/iaqozxfxq/IMG-1718980610773_dJe8eKzOS.jpg',
+      image:
+        'https://storage.cloud.google.com/plane-ticket-be.firebasestorage.app/public/8ccd893d-dc0f-4b02-8a0a-a956a23577a8.jpg',
       continent: 'ASIA'
     },
     {
       id: '2515393b-108d-451e-a919-2bfa2e1c336f',
-      type: 'INTERNATIONAL',
-      code: 'HND',
       name: 'Tokyo International Airport',
       city: 'Tokyo',
-      image: 'https://ik.imagekit.io/iaqozxfxq/IMG-1718888434678_0C9pPslPg.jpg',
+      code: 'HND',
+      type: 'INTERNATIONAL',
+      image:
+        'https://storage.cloud.google.com/plane-ticket-be.firebasestorage.app/public/4809e305-1305-4b15-9f6f-0d414eab66fd.jpg',
       continent: 'ASIA'
     },
     {
+      id: '9cdaed4b-6e39-45c8-8645-39b11b299f8b',
       name: 'Abu Dhabi International Airport',
       city: 'Abu Dhabi',
       code: 'AUH',
       type: 'INTERNATIONAL',
-      image: 'https://ik.imagekit.io/iaqozxfxq/IMG-1718888434678_0C9pPslPg.jpg',
-      continent: 'ASIA',
-      id: '9cdaed4b-6e39-45c8-8645-39b11b299f8b'
+      image:
+        'https://storage.cloud.google.com/plane-ticket-be.firebasestorage.app/public/794bd40d-6ad8-41f1-968b-bfad00408bea.jpg',
+      continent: 'ASIA'
     },
     {
       id: 'f5eeb781-e9cd-4550-877e-be29975a6872',
       name: 'Melbourne Airport',
-      city: 'Melbourne ',
+      city: 'Melbourne',
       code: 'MEL',
       type: 'INTERNATIONAL',
-      image: 'https://ik.imagekit.io/iaqozxfxq/IMG-1718888434678_0C9pPslPg.jpg',
+      image:
+        'https://storage.cloud.google.com/plane-ticket-be.firebasestorage.app/public/7f6977f6-96db-40ad-b136-2a987c9a3bf6.jpg',
       continent: 'AUSTRALIA'
     },
     {
@@ -84,16 +91,18 @@ export async function seedAirport() {
       city: 'Sydney ',
       code: 'SYD',
       type: 'INTERNATIONAL',
-      image: 'https://ik.imagekit.io/iaqozxfxq/IMG-1718888434678_0C9pPslPg.jpg',
+      image:
+        'https://storage.cloud.google.com/plane-ticket-be.firebasestorage.app/public/f4598df7-8f8a-40ae-989c-187d96ca0b02.jpg',
       continent: 'AUSTRALIA'
     },
     {
       id: 'be494d8a-bc14-4bad-94f9-7b9cf35a7883',
-      name: 'Schiphol',
+      name: 'Schiphol International Airport',
       city: 'Amsterdam',
       code: 'AMS',
       type: 'INTERNATIONAL',
-      image: 'https://ik.imagekit.io/iaqozxfxq/IMG-1718888482824_PieEd39tA.jpg',
+      image:
+        'https://storage.cloud.google.com/plane-ticket-be.firebasestorage.app/public/3512ecb5-297a-429b-ad05-291c7cdb41fd.jpg',
       continent: 'EUROPE'
     },
     {
@@ -101,9 +110,9 @@ export async function seedAirport() {
       name: 'Keflavík International Airport',
       city: 'Islandia',
       code: 'KEF',
-      image:
-        'https://ik.imagekit.io/iaqozxfxq/IMG-1719233019681_xhFetRhU_.jfif',
       type: 'INTERNATIONAL',
+      image:
+        'https://storage.cloud.google.com/plane-ticket-be.firebasestorage.app/public/668f92bb-227c-4b02-9a9c-7386dded7adf.jpg',
       continent: 'EUROPE'
     },
     {
@@ -112,7 +121,8 @@ export async function seedAirport() {
       city: 'Los Angeles',
       code: 'LAX',
       type: 'INTERNATIONAL',
-      image: 'https://ik.imagekit.io/iaqozxfxq/IMG-1718888534393_NHshwCyEx.jpg',
+      image:
+        'https://storage.cloud.google.com/plane-ticket-be.firebasestorage.app/public/bca2cfa0-0921-4065-b8d9-5d2f5d4e533b.jpg',
       continent: 'AMERICA'
     },
     {
@@ -121,7 +131,8 @@ export async function seedAirport() {
       city: 'New York',
       code: 'JFK',
       type: 'INTERNATIONAL',
-      image: 'https://ik.imagekit.io/iaqozxfxq/IMG-1718888534393_NHshwCyEx.jpg',
+      image:
+        'https://storage.cloud.google.com/plane-ticket-be.firebasestorage.app/public/5263d340-1812-4153-bffc-555b03cb20f5.jpg',
       continent: 'AMERICA'
     },
     {
@@ -129,9 +140,9 @@ export async function seedAirport() {
       name: 'Cairo International Airport',
       city: 'Cairo',
       code: 'CAI',
-      image:
-        'https://ik.imagekit.io/iaqozxfxq/IMG-1719235677604_frD8aGuyG.jfif',
       type: 'INTERNATIONAL',
+      image:
+        'https://storage.cloud.google.com/plane-ticket-be.firebasestorage.app/public/d981d96d-7e09-4410-8d3a-af5de20747e7.jpg',
       continent: 'AFRICA'
     },
     {
@@ -139,9 +150,9 @@ export async function seedAirport() {
       name: 'O. R. Tambo International Airport',
       city: 'Johannesburg',
       code: 'JNB',
-      image:
-        'https://ik.imagekit.io/iaqozxfxq/IMG-1719235677604_frD8aGuyG.jfif',
       type: 'INTERNATIONAL',
+      image:
+        'https://storage.cloud.google.com/plane-ticket-be.firebasestorage.app/public/a2bdfcd1-5591-4605-8ed8-6e3149029995.jpg',
       continent: 'AFRICA'
     }
   ];
