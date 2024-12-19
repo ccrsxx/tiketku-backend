55c58e89138982bc7b35ab1ef1f9666fe7fa3c36
ccrsxx <aminrisal@gmail.com>
2024-12-19 18:17:45 +0700

    refactor: move static seed data to its own directory

---
+++ src/utils/prisma/seed/schema/data/airline.js
@@ -0,0 +1,68 @@
+/** @import {Prisma} from '@prisma/client' */
+
+/** @type {Prisma.AirlineCreateInput[]} */
+export const airlines = [
+  {
+    id: '7261dc0f-7b3f-4795-b349-18e82e69f839',
+    name: 'Lion Air',
+    code: 'JT',
+    image:
+      'https://storage.googleapis.com/plane-ticket-be.firebasestorage.app/public/fc54bb67-e871-44c5-bc50-40e689cbf653.png'
+  },
+  {
+    id: '75321628-bb7e-43d3-8113-9287db630ff3',
+    name: 'Garuda Indonesia',
+    code: 'GA',
+    image:
+      'https://storage.googleapis.com/plane-ticket-be.firebasestorage.app/public/540b0576-33ea-4f85-aee8-4df5c4dcfed9.jpg'
+  },
+  {
+    id: '56c21f50-9f45-43c2-aacb-f927b3e87af1',
+    name: 'Transnusa',
+    code: 'IR',
+    image:
+      'https://storage.googleapis.com/plane-ticket-be.firebasestorage.app/public/c2ea77f2-f7ca-4c3e-9499-094801fc5bd0.jpg'
+  },
+  {
+    id: '0e32641b-f520-486d-98f4-82de53258fdc',
+    name: 'Pelita Air',
+    code: '6D',
+    image:
+      'https://storage.googleapis.com/plane-ticket-be.firebasestorage.app/public/9ed8e8e4-4de0-4bef-948e-66be229ed661.jpg'
+  },
+  {
+    id: '267f42e0-f0ac-4e2f-9b31-11a69088868b',
+    name: 'Emirates',
+    code: 'EK',
+    image:
+      'https://storage.googleapis.com/plane-ticket-be.firebasestorage.app/public/4475c2fc-dca2-4274-96d4-43fa5068c8e4.png'
+  },
+  {
+    id: '03c22aa6-7ac1-4e2b-ad11-781a0b269deb',
+    name: 'KLM Royal Dutch',
+    code: 'KL',
+    image:
+      'https://storage.googleapis.com/plane-ticket-be.firebasestorage.app/public/1d843e82-0042-4e23-b00b-5fde591f6781.jpg'
+  },
+  {
+    id: 'd3f6bf56-1991-4466-8fa8-733cda833aaf',
+    name: 'Singapore Airlines',
+    code: 'SQ',
+    image:
+      'https://storage.googleapis.com/plane-ticket-be.firebasestorage.app/public/46837687-6d37-4817-865e-3f57448fc783.jpg'
+  },
+  {
+    id: '80eabbc8-9124-4f90-8d72-2b215ed01dfd',
+    name: 'Citilink',
+    code: 'QG',
+    image:
+      'https://storage.googleapis.com/plane-ticket-be.firebasestorage.app/public/26ebb430-47e3-48ab-8677-c3ebf1890154.jpg'
+  },
+  {
+    id: '310c614d-df6b-4a75-b6b6-2685b155d5d3',
+    name: 'Qatar Airways',
+    code: 'QR',
+    image:
+      'https://storage.googleapis.com/plane-ticket-be.firebasestorage.app/public/84ae012b-9035-47d2-af2e-e41cb35225ac.jpg'
+  }
+];
